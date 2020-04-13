d89d23eca8d016da8518a8430d5cb0bc63a59b65
LiamPKenna <liampkenna@gmail.com>
2020-04-13 14:19:31 -0700

    Simplify naming and adjust theme

--- lib/main.dart
+++ lib/main.dart
@@ -8,36 +8,26 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // List<Todo> todos = List<Todo>();
-    // DbHelper helper = DbHelper();
-    // helper
-    //     .initializeDb()
-    //     .then((result) => helper.getTodos().then((result) => todos = result));
-    // DateTime today = DateTime.now();
-    // Todo todo =
-    //     Todo("Buy Melons", 3, today.toString(), "And make sure they are good");
-    // helper.insertTodo(todo);
-
     return MaterialApp(
       title: 'Todos',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: new MyHomePage(title: "Todos"),
+      home: new Home(title: "Todos"),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
