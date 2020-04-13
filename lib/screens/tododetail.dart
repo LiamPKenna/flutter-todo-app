414874fe6a130f721a5e7c15b858fd4d24e7e2e4
LiamPKenna <liampkenna@gmail.com>
2020-04-13 14:22:22 -0700

    Finish connecting CRUD functionality

--- lib/screens/tododetail.dart
+++ lib/screens/tododetail.dart
@@ -3,6 +3,17 @@ import 'package:todo/model/todo.dart';
 import 'package:todo/util/dbhelper.dart';
 import 'package:intl/intl.dart';
 
+DbHelper helper = DbHelper();
+final List<String> choices = const <String>[
+  'Save Todo & Back',
+  'Delete Todo',
+  'Back To List'
+];
+
+const mnuSave = 'Save Todo & Back';
+const mnuDelete = 'Delete Todo';
+const mnuBack = 'Back To List';
+
 class TodoDetail extends StatefulWidget {
   final Todo todo;
   TodoDetail(this.todo);
@@ -14,7 +25,7 @@ class TodoDetail extends StatefulWidget {
 class TodoDetailState extends State {
   Todo todo;
   TodoDetailState(this.todo);
-  final _priorities = ["High", "Medium", "Low", "Really Low"];
+  final _priorities = ["High", "Medium", "Low", "Nevermind"];
   String _priority = "Low";
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
@@ -28,6 +39,19 @@ class TodoDetailState extends State {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text(todo.title),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+            onSelected: select,
+            itemBuilder: (BuildContext context) {
+              return choices.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
+          )
+        ],
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 35.0, left: 10.0, right: 10.0),
@@ -38,6 +62,7 @@ class TodoDetailState extends State {
                 TextField(
                   controller: titleController,
                   style: textStyle,
+                  onChanged: (value) => this.updateTitle(),
                   decoration: InputDecoration(
                       labelText: "Title",
                       labelStyle: textStyle,
@@ -50,6 +75,7 @@ class TodoDetailState extends State {
                   child: TextField(
                     controller: descriptionController,
                     style: textStyle,
+                    onChanged: (value) => this.updateDescription(),
                     decoration: InputDecoration(
                         labelText: "Description",
                         labelStyle: textStyle,
@@ -67,13 +93,82 @@ class TodoDetailState extends State {
                           );
                         }).toList(),
                         style: textStyle,
-                        value: "Low",
-                        onChanged: null)),
+                        value: retrievePriority(todo.priority),
+                        onChanged: (value) => updatePriority(value))),
               ],
             )
           ],
         ),
       ),
     );
+  }
+
+  void select(String value) async {
+    int result;
+    switch (value) {
+      case mnuSave:
+        save();
+        break;
+      case mnuDelete:
+        Navigator.pop(context, true);
+        if (todo.id == null) return;
+        result = await helper.deleteTodo(todo.id);
+        if (result != 0) {
+          AlertDialog alertDialog = AlertDialog(
+            title: Text("Delete Todo"),
+            content: Text("The Todo has been deleted"),
+          );
+          showDialog(context: context, builder: (_) => alertDialog);
+        }
+        break;
+      case mnuBack:
+        Navigator.pop(context, true);
+        break;
+    }
+  }
+
+  void save() {
+    todo.date = new DateFormat.yMd().format(DateTime.now());
+    if (todo.id != null) {
+      helper.updateTodo(todo);
+    } else {
+      helper.insertTodo(todo);
+    }
+    Navigator.pop(context, true);
+  }
+
+  void updatePriority(String value) {
+    switch (value) {
+      case "High":
+        todo.priority = 1;
+        break;
+      case "Medium":
+        todo.priority = 2;
+        break;
+      case "Low":
+        todo.priority = 3;
+        break;
+      case "Nevermind":
+        todo.priority = 4;
+        break;
+      default:
+        todo.priority = 3;
+        break;
+    }
+    setState(() {
+      _priority = value;
+    });
+  }
+
+  String retrievePriority(int value) {
+    return _priorities[value - 1];
+  }
+
+  void updateTitle() {
+    todo.title = titleController.text;
+  }
+
+  void updateDescription() {
+    todo.description = descriptionController.text;
   }
 }
