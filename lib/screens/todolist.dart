7b9038f3e1b4389665c2a31231c851a247b5df23
LiamPKenna <liampkenna@gmail.com>
2020-04-13 11:33:13 -0700

    Create todo list screen

---
+++ lib/screens/todolist.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:todo/model/todo.dart';
+import 'package:todo/screens/tododetail.dart';
+import 'package:todo/util/dbhelper.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  DbHelper helper = DbHelper();
+  List<Todo> todos;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (todos == null) {
+      todos = List<Todo>();
+      getData();
+    }
+    return Scaffold(
+      body: todoListItems(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => debugPrint("Press!!!"),
+        tooltip: "Add new Todo",
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView todoListItems() {
+    return ListView.builder(
+        itemBuilder: (BuildContext context, int position) {
+          return Card(
+            color: Colors.white,
+            elevation: 2.0,
+            child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: getColor(this.todos[position].priority),
+                  child: Text(this.todos[position].priority.toString()),
+                ),
+                title: Text(this.todos[position].title),
+                subtitle: Text(this.todos[position].date),
+                onTap: () {
+                  debugPrint(
+                      "Tapped on: " + this.todos[position].id.toString());
+                  navigateToDetail(this.todos[position]);
+                }),
+          );
+        },
+        itemCount: count);
+  }
+
+  void getData() {
+    final dbFuture = helper.initializeDb();
+    dbFuture.then((result) {
+      final todosFuture = helper.getTodos();
+      todosFuture.then((result) {
+        List<Todo> todoList = List<Todo>();
+        count = result.length;
+        for (int i = 0; i < count; i++) {
+          todoList.add(Todo.fromObject(result[i]));
+          debugPrint(todoList[i].title);
+        }
+        setState(() {
+          todos = todoList;
+          count = count;
+        });
+        debugPrint("Items: " + count.toString());
+      });
+    });
+  }
+
+  Color getColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.yellow;
+        break;
+      case 3:
+        return Colors.green;
+        break;
+      case 4:
+        return Colors.grey;
+        break;
+      default:
+        return Colors.grey;
+    }
+  }
+
+  void navigateToDetail(Todo todo) async {
+    bool result = await Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => TodoDetail(todo)),
+    );
+  }
+}
